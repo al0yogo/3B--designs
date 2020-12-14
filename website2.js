b976e24316bc885518cd80cea61499f6b7b4ed3a
al0yogo <75591388+al0yogo@users.noreply.github.com>
2020-12-13 18:12:00 -0800

    Update website2.js

--- website2.js
+++ website2.js
@@ -54,3 +54,12 @@ function openTab(evt, tabName){
 window.onload = openTab()
 
 
+function openWin() {
+  myWindow = window.open("", "", "width=100, height=100");  // Opens a new window
+}
+
+function resizeWin() {
+  myWindow.resizeTo(250, 250);                             // Resizes the new window
+  myWindow.focus();                                        // Sets focus to the new window
+}
+
